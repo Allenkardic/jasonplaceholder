8f9c466fa04ca8fc96beb1149f055dfbf9ac1fae
Aliakwe <aliakwe@yahoo.com>
2020-01-06 19:47:24 +0100

    Updates

---
+++ precache-manifest.2fd0268237bffaf03f40e159e41b592a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c804ff1276770612d0fe3391921629",
+    "url": "/jasonplaceholder/index.html"
+  },
+  {
+    "revision": "ef007f63ee1d8379b2c5",
+    "url": "/jasonplaceholder/static/css/main.ba8b52d8.chunk.css"
+  },
+  {
+    "revision": "20f03fe0d2f98d490896",
+    "url": "/jasonplaceholder/static/js/2.2ca1c86b.chunk.js"
+  },
+  {
+    "revision": "95c534deb415fe087260dab4786e3626",
+    "url": "/jasonplaceholder/static/js/2.2ca1c86b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef007f63ee1d8379b2c5",
+    "url": "/jasonplaceholder/static/js/main.190388bb.chunk.js"
+  },
+  {
+    "revision": "7bb1582d0fa7887d5086",
+    "url": "/jasonplaceholder/static/js/runtime-main.9acab12f.js"
+  }
+]);
